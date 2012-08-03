0ba4d78b04e13791cae7e7f69a5fc875c8839214
Alexander Fortin <alexander.fortin@gmail.com>
2012-08-03 17:24:43 +0200

    Add RabbitmqTransport, first draft

--- beaver/transports.py
+++ beaver/transports.py
@@ -4,6 +4,7 @@ import socket
 import ujson as json
 import urlparse
 import zmq
+import pika
 
 from datetime import datetime
 
@@ -94,3 +95,56 @@ class RedisTransport(Transport):
                 '@message': line.strip(os.linesep),
             })
             self.redis.lpush(self.namespace, json_msg)
+
+
+class RabbitmqTransport(Transport):
+
+    def __init__(self):
+        # Create our connection object
+        rabbitmq_address = os.environ.get("RABBITMQ_HOST", "localhost")
+        rabbitmq_port    = os.environ.get("RABBITMQ_PORT", 5672)
+        rabbitmq_vhost   = os.environ.get("RABBITMQ_VHOST", "/")
+        rabbitmq_user    = os.environ.get("RABBITMQ_USERNAME", 'guest')
+        rabbitmq_pass    = os.environ.get("RABBITMQ_PASSWORD", 'guest')
+        self.rabbitmq_rk = os.environ.get("RABBITMQ_ROUTING_KEY", '')
+        self.rabbitmq_queue = os.environ.get("RABBITMQ_ROUTING_QUEUE", 'logstash-queue')
+
+
+        credentials = pika.PlainCredentials(
+            rabbitmq_user,
+            rabbitmq_pass
+        )
+        parameters = pika.connection.ConnectionParameters(
+            credentials=credentials,
+            host=rabbitmq_address,
+            port=rabbitmq_port,
+            virtual_host=rabbitmq_vhost
+        )
+        connection = pika.adapters.BlockingConnection(parameters)
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue=self.rabbitmq_queue)
+
+        self.current_host = socket.gethostname()
+
+
+    def callback(self, filename, lines):
+        timestamp = datetime.now().isoformat()
+        for line in lines:
+            json_msg = json.dumps({
+                '@source': "file://{0}{1}".format(self.current_host, filename),
+                '@type': "file",
+                '@tags': [],
+                '@fields': {},
+                '@timestamp': timestamp,
+                '@source_host': self.current_host,
+                '@source_path': filename,
+                '@message': line.strip(os.linesep),
+            })
+            self.channel.basic_publish(exchange='',
+                routing_key=self.rabbitmq_rk,
+                body=json_msg,
+                properties=BasicProperties(
+                    content_type="text/json",
+                    delivery_mode=1
+                )
+            )
