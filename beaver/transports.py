9340d867fa6092b60339e6959041812837d584b0
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-08-05 20:03:55 -0400

    reorder transports

--- beaver/transports.py
+++ beaver/transports.py
@@ -20,44 +20,6 @@ class Transport(object):
         return True
 
 
-class ZmqTransport(Transport):
-
-    def __init__(self):
-        zeromq_address = os.environ.get("ZEROMQ_ADDRESS", "tcp://localhost:2120")
-        zeromq_bind = os.environ.get("BIND", False)
-        self.current_host = socket.gethostname()
-
-        self.ctx = zmq.Context()
-        self.pub = self.ctx.socket(zmq.PUSH)
-
-        if zeromq_bind:
-            self.pub.bind(zeromq_address)
-        else:
-            self.pub.connect(zeromq_address)
-
-    def callback(self, filename, lines):
-        timestamp = datetime.now().isoformat()
-        for line in lines:
-            json_msg = json.dumps({
-                '@source': "file://{0}{1}".format(self.current_host, filename),
-                '@type': "file",
-                '@tags': [],
-                '@fields': {},
-                '@timestamp': timestamp,
-                '@source_host': self.current_host,
-                '@source_path': filename,
-                '@message': line.strip(os.linesep),
-            })
-            self.pub.send(json_msg)
-
-    def interrupt(self):
-        self.pub.close()
-        self.ctx.term()
-
-    def unhandled(self):
-        return True
-
-
 class StdoutTransport(Transport):
 
     def __init__(self):
@@ -94,3 +56,41 @@ class RedisTransport(Transport):
                 '@message': line.strip(os.linesep),
             })
             self.redis.lpush(self.namespace, json_msg)
+
+
+class ZmqTransport(Transport):
+
+    def __init__(self):
+        zeromq_address = os.environ.get("ZEROMQ_ADDRESS", "tcp://localhost:2120")
+        zeromq_bind = os.environ.get("BIND", False)
+        self.current_host = socket.gethostname()
+
+        self.ctx = zmq.Context()
+        self.pub = self.ctx.socket(zmq.PUSH)
+
+        if zeromq_bind:
+            self.pub.bind(zeromq_address)
+        else:
+            self.pub.connect(zeromq_address)
+
+    def callback(self, filename, lines):
+        timestamp = datetime.now().isoformat()
+        for line in lines:
+            json_msg = json.dumps({
+                '@source': "file://{0}{1}".format(self.current_host, filename),
+                '@type': "file",
+                '@tags': [],
+                '@fields': {},
+                '@timestamp': timestamp,
+                '@source_host': self.current_host,
+                '@source_path': filename,
+                '@message': line.strip(os.linesep),
+            })
+            self.pub.send(json_msg)
+
+    def interrupt(self):
+        self.pub.close()
+        self.ctx.term()
+
+    def unhandled(self):
+        return True
