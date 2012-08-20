7f21d42aed6b759d0151656101e00af599ccb9b9
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-08-20 14:20:48 -0400

    use the python logging framework

--- beaver/utils.py
+++ beaver/utils.py
@@ -1,6 +1,7 @@
 import datetime
+import logging
 
 
 def log(line):
     """Log a single line"""
-    print "[{0}] {1}".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), line)
+    logging.info("[%s] %s", datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), line)
