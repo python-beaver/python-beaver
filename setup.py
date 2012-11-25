112ba0d92fac1dcc6ea7701514afff4f763992f3
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-11-24 22:19:25 -0500

    Release version 3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Beaver',
-    version='2',
+    version='3',
     author='Jose Diaz-Gonzalez',
     author_email='support@savant.be',
     packages=['beaver'],
