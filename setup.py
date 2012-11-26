f245f1824287752b8dcea9256eeb237f0c244a66
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-11-25 21:42:23 -0500

    Added python 2.6 to classifiers

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: System :: Logging',
     ],
