0c0a76a1bd1213a13ad2651f1e4606a5f64d4fc3
Chris Faulkner <thefaulkner@gmail.com>
2012-12-06 15:20:50 -0800

    Include changelog in setup.

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(
         'Topic :: System :: Logging',
     ],
     description='python daemon that munches on logs and sends their contents to logstash',
-    long_description=open('README.rst').read(),
+    long_description=open('README.rst').read() + '\n\n' +
+                     open('CHANGES.rst').read(),
     tests_require=["nose",],
     test_suite="nose.collector",
     install_requires=[
