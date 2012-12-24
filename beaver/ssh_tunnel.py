fc2c2d339aafeed7497aa8d3249c00866f50e0b7
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-12-24 18:12:32 -0500

    Added docstrings to ssh_tunnel module

--- beaver/ssh_tunnel.py
+++ beaver/ssh_tunnel.py
@@ -3,35 +3,42 @@ import signal
 import subprocess
 
 
-def create_ssh_tunnel(beaver_config):
+def create_ssh_tunnel(beaver_config, logger=None):
+    """Returns a BeaverSshTunnel object if the current config requires us to"""
     if not beaver_config.use_ssh_tunnel():
         return None
 
+    logger.info("Proxying transport using through local ssh tunnel")
     return BeaverSshTunnel(beaver_config)
 
 
 class BeaverSubprocess:
+    """General purpose subprocess wrapper"""
 
     def __init__(self, beaver_config):
-        # Child classes should build a subprocess via the following method:
-        #
-        #   self._subprocess = subprocess.Popen(cmd, stdout=subprocess.PIPE, preexec_fn=os.setsid)
-        #
-        # This will allow us to attach a session id to the spawned child, allowing
-        # us to send a SIGTERM to the process on close
+        """Child classes should build a subprocess via the following method:
+
+           self._subprocess = subprocess.Popen(cmd, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+
+        This will allow us to attach a session id to the spawned child, allowing
+        us to send a SIGTERM to the process on close
+        """
         self._subprocess = None
 
     def poll(self):
+        """Poll attached subprocess until it is available"""
         if self._subprocess is not None:
             self._subprocess.poll()
 
     def close(self):
+        """Close child subprocess"""
         if self._subprocess is not None:
             os.killpg(self._subprocess.pid, signal.SIGTERM)
             self._subprocess = None
 
 
 class BeaverSshTunnel(BeaverSubprocess):
+    """SSH Tunnel Subprocess Wrapper"""
 
     def __init__(self, beaver_config):
         key_file = beaver_config.get('ssh_key_file')
