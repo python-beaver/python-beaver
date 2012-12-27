e162c303625f0436a7f9b35b0ee3a871606b2713
Norman Joyner <norman.joyner@gmail.com>
2012-12-26 23:52:23 -0500

    implement redis auth support

--- beaver/redis_transport.py
+++ beaver/redis_transport.py
@@ -12,10 +12,11 @@ class RedisTransport(beaver.transport.Transport):
         super(RedisTransport, self).__init__(beaver_config, file_config, logger=logger)
 
         redis_url = beaver_config.get('redis_url')
+        redis_password = beaver_config.get('redis_password')
         _url = urlparse.urlparse(redis_url, scheme="redis")
         _, _, _db = _url.path.rpartition("/")
 
-        self._redis = redis.StrictRedis(host=_url.hostname, port=_url.port, db=int(_db), socket_timeout=10)
+        self._redis = redis.StrictRedis(host=_url.hostname, port=_url.port, password=redis_password, db=int(_db), socket_timeout=10)
         self._redis_namespace = beaver_config.get('redis_namespace')
 
         wait = 0
