23dd9a5c5a94f762d92e500be1d067387b2b9801
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-03-29 14:07:51 -0400

    added support for pubsub zmq

--- beaver/config.py
+++ beaver/config.py
@@ -39,6 +39,7 @@ class BeaverConfig():
             'udp_host': os.environ.get('UDP_HOST', '127.0.0.1'),
             'udp_port': os.environ.get('UDP_PORT', '9999'),
             'zeromq_address': os.environ.get('ZEROMQ_ADDRESS', 'tcp://localhost:2120'),
+            'zeromq_pattern': "push",
             'zeromq_hwm': os.environ.get('ZEROMQ_HWM', ''),
 
             # exponential backoff
--- beaver/zmq_transport.py
+++ beaver/zmq_transport.py
@@ -12,13 +12,16 @@ class ZmqTransport(beaver.transport.Transport):
         zeromq_address = beaver_config.get('zeromq_address')
 
         self._ctx = zmq.Context()
-        self._pub = self._ctx.socket(zmq.PUSH)
+        if beaver_config.get('zeromq_pattern') == 'pub':
+            self._pub = self._ctx.socket(zmq.PUB)
+        else:
+            self._pub = self._ctx.socket(zmq.PUSH)
 
         zeromq_hwm = beaver_config.get('zeromq_hwm')
         if zeromq_hwm:
             self._pub.hwm = zeromq_hwm
 
-        if (beaver_config.get('mode') == "bind"):
+        if beaver_config.get('mode') == 'bind':
             self._pub.bind(zeromq_address)
         else:
             self._pub.connect(zeromq_address)
