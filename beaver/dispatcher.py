5e2f3d115ae987588c701962c26ba0baea1c8a30
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-04-07 17:27:17 -0400

    Fix import of REOPEN_FILES constant in dispatcher.py

--- beaver/dispatcher.py
+++ beaver/dispatcher.py
@@ -7,8 +7,8 @@ import sys
 from beaver.config import FileConfig, BeaverConfig
 from beaver.queue import run_queue
 from beaver.ssh_tunnel import create_ssh_tunnel
-from beaver.utils import setup_custom_logger
-from beaver.worker import Worker, REOPEN_FILES
+from beaver.utils import setup_custom_logger, REOPEN_FILES
+from beaver.worker import Worker
 
 
 def run(args):
