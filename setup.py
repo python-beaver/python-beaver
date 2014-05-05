f41ca756ac8912d7da31b85bdaa5405ca65c914b
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-05-05 03:40:28 -0400

    Add debian packaging based on dh-virtualenv

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# Copyright (c) 2012 Jose Diaz-Gonzalez
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import sys
 
 from beaver import __version__
@@ -28,8 +50,14 @@ setup(
     name='Beaver',
     version=__version__,
     author='Jose Diaz-Gonzalez',
-    author_email='support@savant.be',
-    packages=['beaver', 'beaver.dispatcher', 'beaver.tests', 'beaver.transports', 'beaver.worker'],
+    author_email='email@josediazgonzalez.com',
+    packages=[
+        'beaver',
+        'beaver.dispatcher',
+        'beaver.tests',
+        'beaver.transports',
+        'beaver.worker'
+    ],
     scripts=['bin/beaver'],
     url='http://github.com/josegonzalez/beaver',
     license='LICENSE.txt',
